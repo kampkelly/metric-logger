12d01b5cc54ecca623f4245b6721c48c050925f5
Runor Adjekpiyede <kampkellykeys@gmail.com>
2020-06-13 13:51:44 +0100

    view metric sum

--- src/routes/metric.js
+++ src/routes/metric.js
@@ -5,6 +5,7 @@ import metricController from '../controllers/metricController';
 
 const metricRouter = express.Router();
 
+metricRouter.get('/metric/:key/sum', metricController.getMetricSum);
 metricRouter.post('/metric/:key', metricValidator, metricController.addMetric);
 
 export default metricRouter;
